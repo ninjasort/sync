d43fbdb43d4cc19bd288bfa0b9dc3e4ff2fd4959
Cameron Roe <cameron@indieambassador.com>
2013-03-04 12:17:38 -0500

    updated kit

--- custom.js
+++ custom.js
@@ -20,7 +20,7 @@ var lastDrawTime = -1;
 var timeoutId;
 var pitch = 1;
 var sliderVal;
-var kMaxSwing = .08;
+var kMaxSwing = 0.08;
 var currentKit;
 var rec;
 var instruments = ['kick', 'snare', 'hihat', 'tom1', 'tom2', 'tom3'];
@@ -58,6 +58,66 @@ var beatReset = {
 	"rhythm6":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 };
 
+var funkyHouse = {
+    "kitIndex":0,
+    "effectIndex":0,
+    "tempo":126,
+    "swingFactor":0.5,
+    "effectMix":0.25,
+    "kickPitchVal":0.5,
+    "snarePitchVal":0.5,
+    "hihatPitchVal":0.5,
+    "tom1PitchVal":0.5,
+    "tom2PitchVal":0.5,
+    "tom3PitchVal":0.5,
+    "rhythm1":[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0],
+    "rhythm2":[0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+    "rhythm3":[0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0],
+    "rhythm4":[0,0,0,1,0,0,1,0,0,0,0,0,0,0,1,0],
+    "rhythm5":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "rhythm6":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+};
+
+var hiphop = {
+    "kitIndex":0,
+    "effectIndex":0,
+    "tempo":83,
+    "swingFactor":0,
+    "effectMix":0.25,
+    "kickPitchVal":0.5,
+    "snarePitchVal":0.5,
+    "hihatPitchVal":0.5,
+    "tom1PitchVal":0.5,
+    "tom2PitchVal":0.5,
+    "tom3PitchVal":0.5,
+    "rhythm1":[1,0,0,1,0,0,1,0,1,0,0,0,0,0,0,0],
+    "rhythm2":[0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+    "rhythm3":[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+    "rhythm4":[0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0],
+    "rhythm5":[0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0],
+    "rhythm6":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+};
+
+var dubstep = {
+    "kitIndex":0,
+    "effectIndex":0,
+    "tempo":70,
+    "swingFactor":0,
+    "effectMix":0.25,
+    "kickPitchVal":0.5,
+    "snarePitchVal":0.5,
+    "hihatPitchVal":0.5,
+    "tom1PitchVal":0.5,
+    "tom2PitchVal":0.5,
+    "tom3PitchVal":0.5,
+    "rhythm1":[1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    "rhythm2":[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    "rhythm3":[1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    "rhythm4":[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+    "rhythm5":[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "rhythm6":[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+};
+
 function cloneBeat(source) {
     var beat = new Object();
     
@@ -78,14 +138,60 @@ function cloneBeat(source) {
     beat.rhythm4 = source.rhythm4.slice(0);
     beat.rhythm5 = source.rhythm5.slice(0);
     beat.rhythm6 = source.rhythm6.slice(0);
-    
     return beat;
+}
+
+function clearBeat(ins){
+
+
+    if(ins === 0){
+        theBeat.rhythm1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    }else if(ins === 1){
+        theBeat.rhythm2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    }else if(ins === 2){
+        theBeat.rhythm3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    }else if(ins === 3){
+        theBeat.rhythm4 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    }else if(ins === 4){
+        theBeat.rhythm5 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    }else if(ins === 5){
+        theBeat.rhythm6 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    }
+    for(i=0; i < loopLength; i++){
+        drawNote(0, i, ins);   
+    }
 }
 
 // theBeat is t3he object representing the current beat/groove
 // ... it is saved/loaded via JSON
 var theBeat = cloneBeat(beatReset);
 
+function funkyDemo(){
+    loadBeat(funkyHouse);
+    var kit = new Kit();
+    kit.load();
+    currentKit = kit;
+    $('.pitch').val(1).trigger('change');
+    $('.tempo').val(126).trigger('change');
+    $('.swing').val(5).trigger('change');
+    console.log("funky");
+}
+
+function hiphopDemo(){
+    loadBeat(hiphop);
+    var kit = new Kit();
+    kit.load('hiphop');
+    currentKit = kit;
+    console.log("hiphop");
+}
+
+function dubstepDemo(){
+    loadBeat(dubstep);
+    var kit = new Kit();
+    kit.load('dubstep');
+    currentKit = kit;
+    console.log("dubbb");
+}
 
 function Kit(){
 
@@ -96,23 +202,46 @@ function Kit(){
     this.tom2Buffer = null;
     this.tom3Buffer = null;
 
+    this.kickMuted = false;
+    this.snareMuted = false;
+    this.hihatMuted = false;
+    this.tom1Muted = false;
+    this.tom2Muted = false;
+    this.tom3Muted = false;
+
 	this.startedLoading = false;
 	this.isLoaded = false;
 	this.isPlaying = false;
 
 }
 
-Kit.prototype.load = function(){
+Kit.prototype.load = function(kitPreset){
 	if(this.startedLoading)
 		return;
 	this.startedLoading = true;
 
-	var kick = "samples/909bd4.wav";
-	var snare = "samples/909clap2.wav";
-	var hihat = "samples/909ophat1.wav";
-    var tom1 = "samples/TOM04L.wav";
-    var tom2 = "samples/TOM04M.wav";
-    var tom3 = "samples/TOM05H.wav";
+    if(kitPreset === 'dubstep'){
+        var kick = "samples/dubstepKick.wav";
+        var snare = "samples/dubstepClap.wav";
+        var hihat = "samples/dubstepHH.wav";
+        var tom1 = "samples/dubstepTom.wav";
+        var tom2 = "samples/dubstepTom1.wav";
+        var tom3 = "samples/dubstepTom2.wav";   
+    }else if(kitPreset === 'hiphop'){
+        var kick = "samples/hiphopKick.wav";
+        var snare = "samples/hiphopSnare.wav";
+        var hihat = "samples/hiphopHH.wav";
+        var tom1 = "samples/hiphopTom.wav";
+        var tom2 = "samples/hiphopTom1.wav";
+        var tom3 = "samples/hiphopTom2.wav";
+    }else{
+        var kick = "samples/909bd4.wav";
+        var snare = "samples/909clap2.wav";
+        var hihat = "samples/909ophat1.wav";
+        var tom1 = "samples/TOM04L.wav";
+        var tom2 = "samples/TOM04M.wav";
+        var tom3 = "samples/TOM05H.wav";
+    }
 
 	this.loadSample(0, kick, false);
 	this.loadSample(1, snare, false);
@@ -153,7 +282,6 @@ function loadAssets(){
 
 	kit.isLoaded = true;
 	currentKit = kit;
-
 }
 
 function playSound(buffer, noteTime){
@@ -176,7 +304,6 @@ function playSound(buffer, noteTime){
         // rec = new Recorder(buffer.inputBuffer, cfg);
         // rec.record();
 
-		
         voice.start(noteTime);
         
 }
@@ -192,30 +319,30 @@ function schedule() {
         var contextPlayTime = noteTime + startTime;
         
         // Kick
-        if (theBeat.rhythm1[rhythmIndex]) {
+        if (theBeat.rhythm1[rhythmIndex] && currentKit.kickMuted === false) {
             playSound(currentKit.kickBuffer, contextPlayTime);
         }
 
         // Snare
-        if (theBeat.rhythm2[rhythmIndex]) {
+        if (theBeat.rhythm2[rhythmIndex] && currentKit.snareMuted === false) {
             playSound(currentKit.snareBuffer, contextPlayTime);
         }
 
         // Hihat
-        if (theBeat.rhythm3[rhythmIndex]) {
+        if (theBeat.rhythm3[rhythmIndex] && currentKit.hihatMuted === false) {
             playSound(currentKit.hihatBuffer, contextPlayTime);
         }
 
         // Tom1    
-        if (theBeat.rhythm4[rhythmIndex]) {
+        if (theBeat.rhythm4[rhythmIndex] && currentKit.tom1Muted === false) {
             playSound(currentKit.tom1Buffer, contextPlayTime);
         }
         // Tom2
-        if (theBeat.rhythm5[rhythmIndex]) {
+        if (theBeat.rhythm5[rhythmIndex] && currentKit.tom2Muted === false) {
             playSound(currentKit.tom2Buffer, contextPlayTime);
         }
         // Tom3
-        if (theBeat.rhythm6[rhythmIndex]) {
+        if (theBeat.rhythm6[rhythmIndex] && currentKit.tom3Muted === false) {
             playSound(currentKit.tom3Buffer, contextPlayTime);
         }
 
@@ -233,6 +360,7 @@ function schedule() {
 }
 
 function handlePlay(){
+    currentKit.isPlaying = true;
     noteTime = 0;
 	startTime = context.currentTime + 0.005;
 
@@ -252,6 +380,7 @@ function handlePlay(){
 }
 
 function handleStop(){
+    currentKit.isPlaying = false;
 	clearTimeout(timeoutId);
     // stopRecording(rec);
        // rec.stop();
@@ -275,6 +404,71 @@ function handleStop(){
     drawPlayhead(rhythmIndex);
 }
 
+/* Mutes buttons */
+
+function toggleMute(e){
+    e.preventDefault();
+    
+    var thisMute = e.target.parentNode;
+    thisMute.classList.toggle('active');
+    var instIndex;
+    var beatIndex;
+
+    function paintBtns(instIndex, mute){
+        for(i = 0; i < loopLength; i++){
+            if(beatIndex[i] === 1 && mute){
+                drawNote(2, i, instIndex);
+            }else{
+                drawNote(beatIndex[i], i, instIndex);
+            }
+        }
+    }
+    if(thisMute.classList.contains('active')){
+        console.log("Muted");
+        switch(thisMute.id){
+            case 'kick_mute': currentKit.kickMuted = true; beatIndex = theBeat.rhythm1; instIndex = 0; break;
+            case 'snare_mute': currentKit.snareMuted = true; beatIndex = theBeat.rhythm2; instIndex = 1; break;
+            case 'hihat_mute': currentKit.hihatMuted = true; beatIndex = theBeat.rhythm3; instIndex = 2; break;
+            case 'tom1_mute': currentKit.tom1Muted = true; beatIndex = theBeat.rhythm4; instIndex = 3; break;
+            case 'tom2_mute': currentKit.tom2Muted = true; beatIndex = theBeat.rhythm5; instIndex = 4; break;
+            case 'tom3_mute': currentKit.tom3Muted = true; beatIndex = theBeat.rhythm6; instIndex = 5; break;
+        }
+        paintBtns(instIndex, true);
+    }else{
+        console.log("Unmuted");
+        switch(thisMute.id){
+            case 'kick_mute': currentKit.kickMuted = false; beatIndex = theBeat.rhythm1; instIndex = 0; break;
+            case 'snare_mute': currentKit.snareMuted = false; beatIndex = theBeat.rhythm2; instIndex = 1; break;
+            case 'hihat_mute': currentKit.hihatMuted = false; beatIndex = theBeat.rhythm3; instIndex = 2; break;
+            case 'tom1_mute': currentKit.tom1Muted = false; beatIndex = theBeat.rhythm4; instIndex = 3; break;
+            case 'tom2_mute': currentKit.tom2Muted = false; beatIndex = theBeat.rhythm5; instIndex = 4; break;
+            case 'tom3_mute': currentKit.tom3Muted = false; beatIndex = theBeat.rhythm6; instIndex = 5; break;
+        }
+        paintBtns(instIndex, false);
+    }
+}
+
+function handleClear(e){
+    e.preventDefault();
+
+    var thisClear = e.target.parentNode;
+    var clear = thisClear.id;
+    var j;
+    switch(clear){
+        case 'kick_clear': j=0; break;
+        case 'snare_clear': j=1; break;
+        case 'hihat_clear': j=2; break;
+        case 'tom1_clear': j=3; break;
+        case 'tom2_clear': j=4; break;
+        case 'tom3_clear': j=5; break;
+    }
+    
+    clearBeat(j);
+
+}
+
+/* Start the engine! */
+
 function init(){
 
 	context = new webkitAudioContext();
@@ -286,15 +480,31 @@ function init(){
 
 }
 
+function onKeyDown(e){
+    if(e.keyCode === 32){
+        if(currentKit.isPlaying){
+            handleStop();
+        }else{
+            handlePlay();
+        }
+    }else if(e.keyCode === 67){
+        resetBeat();
+    }
+}
+
 function initControls(){
 
+    window.addEventListener("keydown", onKeyDown);
     document.getElementById('play').addEventListener('click', handlePlay, false);
     document.getElementById('clear').addEventListener('click', resetBeat, false);
-
+    document.getElementById('funky_house').addEventListener('click', funkyDemo, false);
+    document.getElementById('hiphop').addEventListener('click', hiphopDemo, false);
+    document.getElementById('dubstep').addEventListener('click', dubstepDemo, false);
 }
 
 function initButtons() {        
     var elButton;
+    var elMute;
     var kNumInstruments = instruments.length;
 
     for (i = 0; i < loopLength; ++i) {
@@ -302,6 +512,14 @@ function initButtons() {
                 elButton = document.getElementById(instruments[j] + '_' + i);
             	elButton.addEventListener("mousedown", handleButtonMouseDown, true);
         }
+    }
+    for(i=0; i < kNumInstruments; i++){
+        elMute = document.getElementById(instruments[i] + '_mute');
+        elMute.querySelector('a').addEventListener("mousedown", toggleMute, true);
+    }
+    for(i=0; i < kNumInstruments; i++){
+        elMute = document.getElementById(instruments[i] + '_clear');
+        elMute.querySelector('a').addEventListener("mousedown", handleClear, true);
     }
 }
 
@@ -363,11 +581,11 @@ function handleButtonMouseDown(event) {
 
 function drawNote(drawValue, xindex, yindex) {    
     var elButton = document.getElementById(instruments[yindex] + '_' + xindex);
-
+    var buttonMuted = elButton.classList.contains('muted');
     switch (drawValue) {
-        case 0: elButton.classList.remove('alert'); break;
-        case 1: elButton.classList.add('alert'); break;
-        // case 2: elButton.src = 'images/button_on.png'; break;
+        case 0: elButton.classList.remove('alert'); buttonMuted ? elButton.classList.remove('muted') : null; break;
+        case 1: elButton.classList.add('alert'); buttonMuted ? elButton.classList.remove('muted') : null; break;
+        case 2: elButton.classList.remove('alert'); elButton.classList.add('muted'); break;
     }
 }
 
@@ -389,21 +607,10 @@ function advanceNote() {
 }
 
 function resetBeat(){
-	// var panel = document.getElementById("panel");
-	// var p = panel.firstElementChild;
-	// var spans = p.children;
-
-	// for(var i = 0; i < spans.length; i++){
-
-	// 	var span = spans[i].getElementById("label" + i);
-	// 	span.classList.remove("alert");
-
-	// }
     handleStop();
     loadBeat(beatReset);
-
-	// drawPlayhead(0);
 }
+
 function drawPlayhead(xindex) {
     // create a percentage of the current xindex
     var percentage = (xindex * 100) / 15;
@@ -415,8 +622,6 @@ function drawPlayhead(xindex) {
 }
 function loadBeat(beat) {
     // Check that assets are loaded.
-    if (beat != beatReset && !beat.isLoaded())
-        return false;
 
     handleStop();
 
